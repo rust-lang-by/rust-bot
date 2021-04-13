167265c61d57797807f93458548273358b6b9ac3
Ihar Novik <i.novik@godeltech.com>
2021-04-14 00:12:31 +0300

    Add timing for Rust word parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-use chrono::{DateTime, NaiveDateTime, Utc};
+use chrono::{DateTime, Duration, NaiveDateTime, Utc};
 use regex::Regex;
 use teloxide::prelude::*;
 use teloxide::types::InputFile;
@@ -10,9 +10,11 @@ const STICKER_ID: &str = "CAACAgEAAxkBAAOrYGoytP93yNKPRS6jo39dCGmuXnUAAlcBAAJpej
 const HOURS_PER_DAY: i64 = 24;
 const MINUTES_PER_HOUR: i64 = 60;
 const MIN_TIME_DIFF: i64 = 30;
+const RUST_REGEX: &str = r"\b[RrРр][AaUuАа][CcSsСс][TtТт]\b";
 
 lazy_static! {
-    static ref RE: Regex = Regex::new(r"\b[RrРр][AaUuАа][CcSsСс][TtТт]\b").unwrap();
+    static ref RE: Regex = Regex::new(RUST_REGEX).unwrap();
+    static ref REQ_TIME_DIFF: Duration = Duration::seconds(MIN_TIME_DIFF);
 }
 
 #[tokio::main]
@@ -23,7 +25,6 @@ async fn main() {
 async fn run() {
     teloxide::enable_logging!();
     log::info!("Starting bot...");
-
     let mut last_update: DateTime<Utc> = Utc::now();
     log::info!("last date: {}", last_update);
 
@@ -35,22 +36,31 @@ async fn run() {
         if RE.is_match(input_message) {
             let message_date = message.update.date;
             let curr_native_date = NaiveDateTime::from_timestamp(*&message_date as i64, 0);
-            let curr_date: DateTime<Utc> = DateTime::from_utc(curr_native_date, Utc);
+            log::info!("curr_native_date: {}", curr_native_date);
+            let curr_date = DateTime::from_utc(curr_native_date, Utc);
+            log::info!("curr_date: {}", curr_date);
             let time_diff = curr_date.signed_duration_since(last_update);
-            let username = message.update.from().unwrap().username.as_ref();
+            log::info!("time_diff: {}", time_diff);
 
-            message
-                .answer(format!(
-                    "Hi, {}! You just wrote smth about Rust! \nBe careful, \
+            if time_diff > *REQ_TIME_DIFF {
+                let username = message.update.from().unwrap().username.as_ref();
+
+                message
+                    .answer(format!(
+                        "Hi, {}! You just wrote smth about Rust! \nBe careful, \
                     {}d:{}h:{}m since last incident.",
-                    username.unwrap(),
-                    time_diff.num_days(),
-                    time_diff.num_hours() % HOURS_PER_DAY,
-                    time_diff.num_minutes() % MINUTES_PER_HOUR
-                ))
-                .await?;
+                        username.unwrap(),
+                        time_diff.num_days(),
+                        time_diff.num_hours() % HOURS_PER_DAY,
+                        time_diff.num_minutes() % MINUTES_PER_HOUR
+                    ))
+                    .await?;
+
+                last_update = curr_date;
+            }
         }
 
+        log::info!("last date second: {}", last_update);
         message
             .answer_sticker(InputFile::file_id(STICKER_ID))
             .await?;
