8fc27c6a2591d76427080557acaf7e437f248e9a
Ihar Novik <i.novik@godeltech.com>
2021-04-25 13:41:20 +0300

    Refactoring of main method.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use teloxide::types::InputFile;
 const STICKER_ID: &str = "CAACAgEAAxkBAAOrYGoytP93yNKPRS6jo39dCGmuXnUAAlcBAAJpejEFk0uf6g86yKAeBA";
 const HOURS_PER_DAY: i64 = 24;
 const MINUTES_PER_HOUR: i64 = 60;
-const MIN_TIME_DIFF: i64 = 30;
+const MIN_TIME_DIFF: i64 = 15;
 const RUST_REGEX: &str = r"\b[RrРр][AaUuАа][CcSsСс][TtТт]\b";
 
 #[tokio::main]
@@ -25,14 +25,7 @@ async fn run() {
     let bot = Bot::from_env().auto_send();
     let regex = Regex::new(RUST_REGEX).unwrap();
     let pool = establish_connection().await;
-    let req_time_diff = Duration::seconds(MIN_TIME_DIFF);
-
-    // pool the latest mention time during app initialization
-    let last_mention_time = mention_repository::lead_earliest_mention_time(&pool)
-        .await
-        .unwrap();
-    let mut last_update_time = Utc.from_utc_datetime(&last_mention_time);
-    log::info!("latest mention time: {}", last_update_time);
+    let req_time_diff = Duration::minutes(MIN_TIME_DIFF);
 
     teloxide::repl(bot, move |message| {
         let cloned_pool = pool.clone();
@@ -40,52 +33,72 @@ async fn run() {
         let cloned_time_diff = req_time_diff.clone();
 
         async move {
-            let input_message = message.update.text().unwrap();
+            let input_message = message.update.text().unwrap_or("");
 
             if cloned_regex.is_match(input_message) {
-                let message_date = message.update.date;
-                let curr_native_date = NaiveDateTime::from_timestamp(*&message_date as i64, 0);
-                log::info!("curr_native_date: {}", curr_native_date);
-                let curr_date = DateTime::from_utc(curr_native_date, Utc);
-                log::info!("curr_date: {}", curr_date);
-                let time_diff = curr_date.signed_duration_since(last_update_time);
-                log::info!("time_diff: {}", time_diff);
-
-                if time_diff > cloned_time_diff {
-                    let user = message.update.from().unwrap();
-                    let username = user.username.as_ref().unwrap();
-
-                    message
-                        .reply_to(format!(
-                            "Hi, {}! You just wrote smth about Rust! \nBe careful, \
-                    {}d:{}h:{}m since last incident.",
-                            username,
-                            time_diff.num_days(),
-                            time_diff.num_hours() % HOURS_PER_DAY,
-                            time_diff.num_minutes() % MINUTES_PER_HOUR
-                        ))
-                        .await?;
-
-                    message
-                        .answer_sticker(InputFile::file_id(STICKER_ID))
-                        .await?;
-
-                    last_update_time = curr_date;
-                    mention_repository::insert_mention(&cloned_pool, user.id)
-                        .await
-                        .unwrap();
-                }
+                handle_matched_mention(message, cloned_pool, cloned_time_diff).await;
             }
-
-            log::info!("last date second: {}", last_update_time);
-
             respond(())
         }
     })
     .await;
 }
 
-pub async fn establish_connection() -> Pool<Postgres> {
+async fn handle_matched_mention(
+    message: UpdateWithCx<AutoSend<Bot>, Message>,
+    cloned_pool: PgPool,
+    cloned_time_diff: Duration,
+) {
+    let message_date = message.update.date;
+    let curr_native_date = NaiveDateTime::from_timestamp(*&message_date as i64, 0);
+    let curr_date: DateTime<Utc> = DateTime::from_utc(curr_native_date, Utc);
+    log::info!("mention time: {}", curr_date);
+
+    // pool the latest mention time from db
+    let last_mention_time = mention_repository::lead_earliest_mention_time(&cloned_pool)
+        .await
+        .expect("Can't pool latest mention time");
+    let last_update_time = Utc.from_utc_datetime(&last_mention_time);
+    log::info!("latest update time: {}", last_update_time);
+
+    let time_diff = curr_date.signed_duration_since(last_update_time);
+
+    if time_diff > cloned_time_diff {
+        send_mention_response(message, cloned_pool, time_diff).await;
+    }
+}
+
+async fn send_mention_response(
+    message: UpdateWithCx<AutoSend<Bot>, Message>,
+    cloned_pool: PgPool,
+    time_diff: Duration,
+) {
+    let user = message.update.from().expect("Can't identify user");
+    let username = user.username.as_ref().unwrap();
+
+    message
+        .reply_to(format!(
+            "Hi, {}! You just wrote smth about Rust! \nBe careful, \
+                    {}d:{}h:{}m since last incident.",
+            username,
+            time_diff.num_days(),
+            time_diff.num_hours() % HOURS_PER_DAY,
+            time_diff.num_minutes() % MINUTES_PER_HOUR
+        ))
+        .await
+        .expect("Can't send reply");
+
+    message
+        .answer_sticker(InputFile::file_id(STICKER_ID))
+        .await
+        .expect("Can't send sticker");
+
+    mention_repository::insert_mention(&cloned_pool, user.id)
+        .await
+        .expect("Can't insert mention");
+}
+
+async fn establish_connection() -> Pool<Postgres> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgPool::connect(&database_url)
         .await
--- src/mention_repository.rs
+++ src/mention_repository.rs
@@ -10,7 +10,7 @@ pub async fn lead_earliest_mention_time(pool: &PgPool) -> Result<NaiveDateTime, 
 }
 
 pub async fn insert_mention(pool: &PgPool, user_id: i64) -> Result<PgQueryResult, Error> {
-    sqlx::query("INSERT INTO mentions(user_id) VALUES ($1)  ON CONFLICT (user_id) DO UPDATE SET updated_at = current_timestamp RETURNING user_id")
+    sqlx::query("INSERT INTO mentions(user_id) VALUES ($1)  ON CONFLICT (user_id) DO UPDATE SET updated_at = current_timestamp")
         .bind(user_id)
         .execute(pool)
         .await
