e7a00aadc4429529b7ae2ef8f0bf8779fe810194
i.novik <nolik03@gmail.com>
2023-03-08 14:46:26 +0100

    Add reaction to blazing fast mention. (#121)
    * blazing-fast - add parsing blazing fast msg handling.

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ const HOURS_PER_DAY: i64 = 24;
 const MINUTES_PER_HOUR: i64 = 60;
 const MIN_TIME_DIFF: i64 = 15;
 const RUST_REGEX: &str = r"\b[RrРр][AaUuАа][CcSsСс][TtТт]\b";
+const BLAZING_FAST_REGEX: &str = r"\b[BbБб][LlЛл]\w*\W[FfФф][AaАа]\w*\b";
 
 #[tokio::main]
 async fn main() {
@@ -28,7 +29,8 @@ async fn run() {
     let bot = Bot::from_env();
     let db_pool = establish_connection().await;
     let mention_parameters = MentionParameters {
-        regex: Regex::new(RUST_REGEX).expect("Can't compile regex"),
+        rust_regex: Regex::new(RUST_REGEX).expect("Can't compile regex"),
+        blazing_fast_regex: Regex::new(BLAZING_FAST_REGEX).expect("Can't compile regex"),
         req_time_diff: Duration::minutes(MIN_TIME_DIFF),
     };
 
@@ -42,14 +44,20 @@ async fn run() {
                  db_pool: Pool<Postgres>,
                  bot: Bot| async move {
                     if let Some(message) = msg.text() {
-                        if mention_parameters.regex.is_match(message) {
-                            handle_matched_mention(
-                                bot,
-                                msg,
-                                db_pool,
-                                mention_parameters.req_time_diff,
-                            )
-                            .await;
+                        match message {
+                            m if mention_parameters.rust_regex.is_match(m) => {
+                                handle_rust_matched_mention(
+                                    bot,
+                                    msg,
+                                    db_pool,
+                                    mention_parameters.req_time_diff,
+                                )
+                                .await
+                            }
+                            m if mention_parameters.blazing_fast_regex.is_match(m) => {
+                                handle_bf_matched_mention(bot, msg).await
+                            }
+                            _ => {}
                         }
                     }
                     respond(())
@@ -69,7 +77,16 @@ async fn run() {
         .await;
 }
 
-async fn handle_matched_mention(
+async fn handle_bf_matched_mention(bot: Bot, msg: Message) {
+    bot.send_message(msg.chat.id, "Did you meant Rust? 👉👈".to_string())
+        .reply_to_message_id(msg.id)
+        .message_thread_id(msg.thread_id.unwrap_or(0))
+        .await
+        .map_err(|err| error!("Can't send reply: {:?}", err))
+        .ok();
+}
+
+async fn handle_rust_matched_mention(
     bot: Bot,
     message: Message,
     db_pool: PgPool,
@@ -98,17 +115,17 @@ async fn handle_matched_mention(
     }) = message.kind
     {
         if time_diff > req_time_diff {
-            let message_ids = (message.id, message.chat.id, message.thread_id);
-            send_mention_response(bot, message_ids, time_diff, &username).await;
+            let message_ids = (message.id, message.chat.id, message.thread_id.unwrap_or(0));
+            send_rust_mention_response(bot, message_ids, time_diff, &username).await;
         }
 
         mention_repository::insert_mention(&db_pool, user_id.0 as i64, &username).await;
     }
 }
 
-async fn send_mention_response(
+async fn send_rust_mention_response(
     bot: Bot,
-    message_ids: (MessageId, ChatId, Option<i32>),
+    message_ids: (MessageId, ChatId, i32),
     time_diff: Duration,
     username: &str,
 ) {
@@ -123,14 +140,14 @@ async fn send_mention_response(
             time_diff.num_minutes() % MINUTES_PER_HOUR
         ),
     )
-    .message_thread_id(message_ids.2.unwrap_or(0))
+    .message_thread_id(message_ids.2)
     .reply_to_message_id(message_ids.0)
     .await
     .map_err(|err| error!("Can't send reply: {:?}", err))
     .ok();
 
     bot.send_sticker(message_ids.1, InputFile::file_id(STICKER_ID))
-        .message_thread_id(message_ids.2.unwrap_or(0))
+        .message_thread_id(message_ids.2)
         .await
         .map_err(|err| error!("Can't send a sticker: {:?}", err))
         .ok();
@@ -145,6 +162,7 @@ async fn establish_connection() -> Pool<Postgres> {
 
 #[derive(Clone)]
 struct MentionParameters {
-    regex: Regex,
+    rust_regex: Regex,
+    blazing_fast_regex: Regex,
     req_time_diff: Duration,
 }
