cac55a258f1eb6938a59ebb1dadde4284ef96497
i.novik <nolik03@gmail.com>
2023-04-02 22:47:49 +0200

    gpt-context - add bot gpt context. (#152)

---
+++ src/chat_gpt_repository.rs
@@ -0,0 +1,28 @@
+use crate::chat_gpt_handler::ChatMessage;
+use redis::{aio::MultiplexedConnection, RedisResult};
+
+pub async fn get_context(
+    connection: &MultiplexedConnection,
+    key: &String,
+) -> RedisResult<Vec<ChatMessage>> {
+    let mut con = connection.clone();
+    redis::cmd("LRANGE")
+        .arg(key)
+        .arg(0)
+        .arg(10)
+        .query_async(&mut con)
+        .await
+}
+
+pub async fn set_context(
+    connection: &MultiplexedConnection,
+    key: &String,
+    context: Vec<&ChatMessage>,
+) -> RedisResult<()> {
+    let mut con = connection.clone();
+    redis::cmd("LPUSH")
+        .arg(key)
+        .arg(context)
+        .query_async(&mut con)
+        .await
+}
