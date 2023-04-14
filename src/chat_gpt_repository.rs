6b40fc8888e176919f9fa4cc9d2f4740dfac8300
i.novik <nolik03@gmail.com>
2023-04-14 17:32:59 +0200

    redis-timeout - add redis timeouts. (#172)

--- src/chat_gpt_repository.rs
+++ src/chat_gpt_repository.rs
@@ -1,12 +1,17 @@
 use crate::chat_gpt_handler::ChatMessage;
 use redis::aio::ConnectionManager;
 use redis::{AsyncCommands, RedisResult};
+use tokio::io;
+use tokio::time::error::Elapsed;
+use tokio::time::{timeout, Duration};
+
+const REDIS_TIMEOUT: Duration = Duration::from_secs(1);
 
 pub async fn get_context(
     connection_manager: &mut ConnectionManager,
     key: &String,
 ) -> RedisResult<Vec<ChatMessage>> {
-    connection_manager.lrange(key, 0, 19).await
+    timeout_cmd(REDIS_TIMEOUT, connection_manager.lrange(key, 0, 19)).await
 }
 
 pub async fn set_context(
@@ -14,5 +19,21 @@ pub async fn set_context(
     key: &String,
     context: Vec<&ChatMessage>,
 ) -> RedisResult<()> {
-    redis_connection_manager.lpush(key, context).await
+    timeout_cmd(REDIS_TIMEOUT, redis_connection_manager.lpush(key, context)).await
+}
+
+#[inline]
+pub async fn timeout_cmd<T>(
+    duration: Duration,
+    future: redis::RedisFuture<'_, T>,
+) -> RedisResult<T> {
+    timeout(duration, future)
+        .await
+        .map_err(redis_error_from_elapsed)
+        .and_then(|v| v)
+}
+
+#[inline]
+pub fn redis_error_from_elapsed(_: Elapsed) -> redis::RedisError {
+    redis::RedisError::from(io::Error::from(io::ErrorKind::TimedOut))
 }
